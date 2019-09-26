478e5393eefbb970b2ac269e23b0e4f44ab4bda7
perukas <ankarakottas@gmail.com>
2019-09-26 17:24:56 +0300

    fixed dataloader batch and workers

--- visualize_dataset.py
+++ visualize_dataset.py
@@ -10,9 +10,8 @@ if __name__ == "__main__":
         image_types=[ThreeD60.ImageTypes.COLOR, ThreeD60.ImageTypes.DEPTH, ThreeD60.ImageTypes.NORMAL], longitudinal_rotation=True)
     print("Loaded %d samples." % len(datasets))
     
-    viz = visualization.VisdomImageVisualizer("3D60", "195.251.117.120")
-    dataset_loader = DataLoader(datasets, batch_size=32, shuffle=False, pin_memory=False, num_workers=0)
-    # dataset_loader = DataLoader(datasets, batch_size=32, shuffle=True, pin_memory=False, num_workers=4)
+    viz = visualization.VisdomImageVisualizer("3D60", "195.251.117.121")
+    dataset_loader = DataLoader(datasets, batch_size=32, shuffle=True, pin_memory=False, num_workers=4)
     for i, b in enumerate(dataset_loader):
         viz.show_images_grid(ThreeD60.extract_image(b, ThreeD60.Placements.CENTER, ThreeD60.ImageTypes.COLOR), "all_colors")
         viz.show_depths_grid(ThreeD60.extract_image(b, ThreeD60.Placements.CENTER, ThreeD60.ImageTypes.DEPTH), "all_depths")
